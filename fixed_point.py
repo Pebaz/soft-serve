192faf6ed707ed91b53744df2e30ae69e1c912df
Pebaz <pebazium@gmail.com>
2022-07-19 01:11:29 -0700

    Fixed point to string

---
+++ fixed_point.py
@@ -0,0 +1,40 @@
+
+def Fixed(width, dot):
+    class FixedDecimal:
+        def __init__(self, value):
+            # TODO(pbz): If isinstance(...)
+            self.value = value
+
+        def __float__(self):
+            b4_dot = ('0' * width) + bin(self.value)[2:]
+            b4_dot = b4_dot[len(b4_dot) - dot:]
+            dec = 0.0
+            for i in range(dot):
+                if b4_dot[i] == '1':
+                    dec += 2 ** -(2 ** i)
+
+            int_ = 0
+            for i in range(dot, width):
+                if self.value & (1 << i):
+                    int_ += 2 ** (i - dot)
+
+            return float(int_) + dec
+
+        def __str__(self):
+            if not dot:
+                "It's just an integer at this point"
+                return bin(self.value)[2:]
+            buf = ''
+            for i in range(width):
+                bit = 0b1 << i
+                buf = ('1' if self.value & bit else '0') + buf
+                if i == dot - 1:
+                    buf = '.' + buf
+            return buf
+
+    return FixedDecimal
+
+Fix8 = Fixed(8, 4)
+
+a = Fix8(0b00011100)
+print(float(a), a)
