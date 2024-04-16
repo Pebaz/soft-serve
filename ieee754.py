3971ee5c21518c568c8f950ff3ddac77b04ae236
Pebaz <pebazium@gmail.com>
2024-04-16 02:06:50 -0400

    Integrate tidbytes 0.1.1

--- ieee754.py
+++ ieee754.py
@@ -13,26 +13,11 @@ class Single(Mem[32]):
         return self[1:9]
 
     def mantissa(self) -> Unsigned[23]:
-        # TODO(pbz): Fix indexing: self[9:] failed
-        return self[9:len(self)]
+        return self[9:]
 
     def __add__(self, other: 'Single'):
         is_zero = not self.exponent()
         return self
-
-    def bytes(self, byte_order: Optional[Order] = None) -> bytes:
-        # TODO(pbz): Tidbytes to big_endian_bytes or little_endian_bytes
-        byte_order = byte_order or (L2R, R2L)[sys.byteorder == 'big']
-        it = self.rgn.bytes if byte_order == L2R else reversed(self.rgn.bytes)
-        buffer = b''
-
-        for byte in it:
-            acc = 0
-            for i, bit in enumerate(reversed(byte)):
-                # TODO(pbz): Implement this nifty multiply trick in tidbytes
-                acc |= (1 << i) * bit
-            buffer += bytes([acc])
-        return buffer
 
     def __float__(self):
         "struct.unpack and Single.bytes assume system endianness by default."
@@ -45,5 +30,4 @@ class Single(Mem[32]):
 single1 = Single('10000000')
 single2 = Single('0')
 # print(single1 + single2)
-# TODO(pbz): Fix from_float in tidbytes to suggest using f32 (currently doesn't)
 print(Single(f32(3.14)))
